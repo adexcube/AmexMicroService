3a470c115cb822dccba5ed504acbe6b02553f60e
Adeoye Adesina <adeoyeadesina@Adeoyes-MacBook-Pro.local>
2020-08-12 15:09:09 -0500

    lfkajs

--- src/main/java/edu/miu/eshop/amexbank/Controller/AmexdataController.java
+++ src/main/java/edu/miu/eshop/amexbank/Controller/AmexdataController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.miu.eshop.amexbank.domain.Amexdata;
 import edu.miu.eshop.amexbank.domain.Transaction;
 import edu.miu.eshop.amexbank.service.AmexdataService;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("amex")
 public class AmexdataController {
